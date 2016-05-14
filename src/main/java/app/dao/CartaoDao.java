83e2c73be2e1a98591cff471387c59e0904fd3dc
Leonardo Gomes <leogomes26@hotmail.com>
2016-05-14 19:49:41 -0300

    Disp. novos serviços de Cartoes.

--- src/main/java/app/dao/CartaoDao.java
+++ src/main/java/app/dao/CartaoDao.java
@@ -1,11 +1,15 @@
 package app.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import app.model.Cartao;
+import app.model.Pessoa;
 
 @Repository
 public interface CartaoDao extends JpaRepository<Cartao, Integer>{
   public Cartao findByidCartao(Integer id);
+  public List<Cartao> findBypessoa(Pessoa pessoa);
 }
